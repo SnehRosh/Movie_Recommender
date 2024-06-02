3597e2a33f774aa4a10749a3011ff7ba591e7670
sneha <rosh3267@gmail.com>
2024-06-02 20:50:46 +0530

    update #2

---
+++ home.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+import textblob
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import turtle
+
+
+# Config
+st.set_page_config(
+    layout="centered",
+    page_title="Movie Recommendation",
+    page_icon="🎬",
+    initial_sidebar_state='expanded',)
+
+def load_data(path):
+    df = pd.read_csv('C:/Users/Hp/OneDrive/Desktop/PYDS/Movie_recommender/imdb_raw.csv')    
+    return df
+
+
+with st.spinner('Processing Movies Data'):
+    df=load_data('C:/Users/Hp/OneDrive/Desktop/PYDS/Movie_recommender/imdb_raw.csv')
+with st.container():
+    st.title("Movie Remommendation website")
+
+#Top rated movies
+c1,c2=st.columns(2)
+ft = c1.form('Find the top rated movies here')
+btn = ft.form_submit_button(":red[Click here to get the top rated movies]") 
+if btn: 
+    nd= df[['title','release_year','rating']]
+    top_df = nd.head(10)
+    stop_df = top_df.sort_values(by='rating',ascending=False)
+    top10_mv = stop_df.head(10)
+    btn = st.dataframe(data=top10_mv,
+                           width=1000,
+                           height=400)
+    
+
+#Movie recommendation based on selected movie
+st.subheader("Enter the name of Movie for recommendation")
+c1,c2=st.columns(2)
+movie_list=df.index.tolist()
+mv_dl=c1.text_input('Enter the name of the movie')
+df['data'] = df['title'] + ' ' + df['director'] + ' ' +  df['genre']
+    #remove punctuations - anything that is not a word or a space
+df['data'] = df['data'].str.replace('[^\w\s]','')
+    # Lower case
+df['data'] = df['data'].str.lower()
+def remove_stopwords(text):
+    words = text.split()
+    return" ".join(word for word in words if word not in stopwords.words('english'))
+df['data'] = df['data'].apply(remove_stopwords)
+    #vectorize the data
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df['data']).toarray()
+# similarity matrix
+similarity = cosine_similarity(X, X)
+def get_index_from_title(title):
+    try: 
+        return df[df.title == title].index[0]
+    except:
+        return None
+def recommend_movie(title, Limit = 10):
+    index = get_index_from_title(title)
+    if index is None:
+        return []
+    else:
+        movie_scores = []
+        for i in range(similarity.shape[0]):
+            movie_scores.append((df['title'][i],similarity[index][i]))
+        movie_scores.sort(key=lambda x:x[1],reverse = True)
+        return movie_scores[1:Limit+1]
+if mv_dl:    
+    rm= recommend_movie(mv_dl,5)
+    drm=pd.DataFrame(rm)
+    drm
+from turtle import *
+#bgpic('bckgrd.jpg')
